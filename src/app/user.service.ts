7356cf29b6bfca126198d32b3b49e3d5c3af72b2
Shivank <Shivankkapoor@yahoo.com>
2023-12-25 21:44:52 -0600

    can get user based on username

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
-import PocketBase from 'PocketBase'
+import PocketBase from 'PocketBase';
 import { environment } from '../environments/environment.development';
 import { UserModel } from './models/user.model';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor() {}
 
-  constructor() { }
-
-  async getUsers(): Promise<UserModel[]>{
+  async getUsers(): Promise<UserModel[]> {
     const pb = new PocketBase(environment.baseUrl);
-    const records:UserModel[] = await pb.collection('reviewer').getFullList({
+    const records: UserModel[] = await pb.collection('reviewer').getFullList({
       sort: '-created',
-  });
-  return records;
+    });
+    return records;
+  }
+
+  async getSpecificUser(userName: string) {
+    const pb = new PocketBase(environment.baseUrl);
+    const record: UserModel[] = await pb
+      .collection('reviewer')
+      .getFirstListItem('username="shivank"', {
+        expand: 'relField1,relField2.subRelField',
+      });
+
+    console.log(record);
+    return record;
   }
 }
