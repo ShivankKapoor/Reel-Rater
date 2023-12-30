cb9d925d8fd4a615346ca95645182078d839b380
Shivank <Shivankkapoor@yahoo.com>
2023-12-30 02:15:12 -0600

    get model and service for movies

--- src/app/backend-services/movies.service.ts
+++ src/app/backend-services/movies.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import PocketBase from 'PocketBase';
+import { environment } from '../../environments/environment.development';
+import { MovieModel } from '../models/movies.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,12 @@ import { Injectable } from '@angular/core';
 export class MoviesService {
 
   constructor() { }
+
+  async getAllRatings(): Promise<MovieModel[]> {
+    const pb = new PocketBase(environment.baseUrl);
+    const records: MovieModel[] = await pb.collection('movies').getFullList({
+      sort: '-created',
+    });
+    return records;
+  }
 }
---
+++ src/app/models/movies.model.ts
@@ -0,0 +1,10 @@
+export interface MovieModel {
+    id:             string;
+    collectionId:   string;
+    collectionName: string;
+    created:        string;
+    updated:        string;
+    title:          string;
+    release:        string;
+    genre:          string;
+}
