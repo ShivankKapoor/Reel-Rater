6454ead00741fb47b1519b02ba0ffd4153710322
Shivank <Shivankkapoor@yahoo.com>
2024-01-06 17:46:10 -0600

    Add warning if rating is empty

--- src/app/components/new-review/new-review.component.ts
+++ src/app/components/new-review/new-review.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MovieRatingsService } from '../../backend-services/movie-ratings.service';
 import { InputRatingModel } from '../../models/input-rating.model';
+import { MiniWarningService } from '../../services/warning services/mini-warning.service';
 
 @Component({
   selector: 'app-new-review',
@@ -8,20 +9,28 @@ import { InputRatingModel } from '../../models/input-rating.model';
   styleUrl: './new-review.component.scss',
 })
 export class NewReviewComponent {
-
   title: string = '';
   releaseDate: Date = new Date();
   genre: string = '';
   rating: number = 1;
 
-  constructor(private movieService: MovieRatingsService) { }
+  constructor(private movieService: MovieRatingsService, private warn:MiniWarningService) {}
 
   onRatingChange($event: number) {
     this.rating = $event;
   }
 
   submit() {
-    var movie: InputRatingModel = { title: this.title, genre: this.genre, releaseDate: this.releaseDate, rating: this.rating }
-    this.movieService.publishMovieRating(movie);
+    if (this.title === '' || this.genre === '') {
+      this.warn.openSnackBar("Form cannot be blank","Close");
+    } else {
+      var movie: InputRatingModel = {
+        title: this.title,
+        genre: this.genre,
+        releaseDate: this.releaseDate,
+        rating: this.rating,
+      };
+      this.movieService.publishMovieRating(movie);
+    }
   }
 }
