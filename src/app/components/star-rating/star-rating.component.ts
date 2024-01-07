de40f773cef91607bd309f4f146dfd6d6b9b8384
Shivank <Shivankkapoor@yahoo.com>
2024-01-06 21:13:01 -0600

    Fix hover

--- src/app/components/star-rating/star-rating.component.ts
+++ src/app/components/star-rating/star-rating.component.ts
@@ -8,15 +8,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class StarRatingComponent {
   @Input() rating: number = 0;
   @Output() ratingChange: EventEmitter<number> = new EventEmitter<number>();
+  lastClickedIndex: number = 0;
   hoveredStarIndex: number = 0;
   stars: number[] = [1, 2, 3, 4, 5];
 
   rate(rating: number): void {
     this.rating = rating;
+    this.lastClickedIndex = rating - 1; // Adjust to 0-based index
     this.ratingChange.emit(this.rating);
   }
 
   setHoveredStarIndex(index: number): void {
     this.hoveredStarIndex = index;
+  }
+
+  clearHover(): void {
+    this.hoveredStarIndex = this.lastClickedIndex;
   }
 }
