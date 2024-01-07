a1ef49f98e022f7231f2857a468883891fd7cba8
Shivank <Shivankkapoor@yahoo.com>
2024-01-06 21:11:12 -0600

    Add hover animation to stars

--- src/app/components/star-rating/star-rating.component.ts
+++ src/app/components/star-rating/star-rating.component.ts
@@ -3,16 +3,20 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-star-rating',
   templateUrl: './star-rating.component.html',
-  styleUrl: './star-rating.component.scss'
+  styleUrls: ['./star-rating.component.scss']
 })
 export class StarRatingComponent {
   @Input() rating: number = 0;
   @Output() ratingChange: EventEmitter<number> = new EventEmitter<number>();
-
+  hoveredStarIndex: number = 0;
   stars: number[] = [1, 2, 3, 4, 5];
 
   rate(rating: number): void {
     this.rating = rating;
     this.ratingChange.emit(this.rating);
+  }
+
+  setHoveredStarIndex(index: number): void {
+    this.hoveredStarIndex = index;
   }
 }
