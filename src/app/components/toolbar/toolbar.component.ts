3c32d83c3b16b61826c43469c7bbe7c9d057ca1a
Shivank <Shivankkapoor@yahoo.com>
2024-01-10 02:48:15 -0600

    Add button works

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SidebarControlService } from '../navigation-menu/sidebar-control.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
-
+  constructor(private router:Router, private sidebarControlService: SidebarControlService){}
+  addReview(){
+    this.router.navigate(['add-review']);
+  }
 }
