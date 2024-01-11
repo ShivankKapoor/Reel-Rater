9fcfbd252621efb46ccc219241d1083738b56b4f
Shivank <Shivankkapoor@yahoo.com>
2024-01-10 21:24:04 -0600

    add a service for selection

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { MovieRatingModel } from '../../models/movie-rating.model';
 import { MovieRatingsService } from '../../backend-services/movie-ratings.service';
 import { StringFormattingService } from '../../services/string-formatting/string-formatting.service';
+import { HomeSelectionService } from './home-selection.service';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,11 @@ export class HomeComponent implements OnInit {
   displayedColumns: string[] = ['title', 'releaseDate', 'genre', 'rating'];
   clickedRows = new Set<MovieRatingModel>();
 
-  constructor(public str: StringFormattingService, private ratings: MovieRatingsService) {}
+  constructor(
+    public str: StringFormattingService,
+    private ratings: MovieRatingsService,
+    private selection: HomeSelectionService
+  ) {}
 
   async ngOnInit(): Promise<void> {
     await this.loadRatings();
@@ -29,12 +34,16 @@ export class HomeComponent implements OnInit {
   }
 
   rowClick(row: MovieRatingModel) {
-    this.clickedRows.clear();
-    this.clickedRows.add(row);
+    if (this.selection.hasItem(row)) {
+      this.selection.clearItems();
+    } else {
+      this.selection.clearItems();
+      this.selection.addItem(row);
+    }
   }
 
   isRowClicked(row: MovieRatingModel): boolean {
-    return this.clickedRows.has(row);
+    return this.selection.hasItem(row);
   }
 
   formatDate(date: string) {
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { SidebarControlService } from '../navigation-menu/sidebar-control.service';
+import { HomeSelectionService } from '../home/home-selection.service';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrl: './toolbar.component.scss'
+  styleUrl: './toolbar.component.scss',
 })
 export class ToolbarComponent {
-  constructor(private router:Router, private sidebarControlService: SidebarControlService){}
-  addReview(){
+  public hasSelection: boolean = false;
+  constructor(
+    private router: Router,
+    private sidebarControlService: SidebarControlService,
+    private selection: HomeSelectionService
+  ) {
+    this.selection.dataSet$.subscribe((data) => {
+      if(data.size>0){
+        this.hasSelection=true;
+      }else{
+        this.hasSelection=false;
+      }
+    });
+  }
+  addReview() {
     this.router.navigate(['add-review']);
   }
 }
